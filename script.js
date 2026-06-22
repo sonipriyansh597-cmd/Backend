c8d9cd00f595308e0c5914d9622890e152b187e8
Priyansh <sonipriyansh597@gmail.com>
2026-06-22 10:49:55 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var arr = [1,2,3,4,5];
+arr.forEach(function(val){
+    console.log(val + " hello");
+})
+
+
+var arr = [1,2,3,4];
+var newarr = arr.map(function(val){
+    return val*2;
+})
+console.log(newarr);
+
+
+var arr = [1,2,3,4,5,6];
+var ans = arr.filter(function(val){
+    if(val > 3){ return true;}
+    else return false;
+})
+console.log(ans);
+
+
+var arr = [1,2,3,4,5];
+var ans = arr.find(function(val){
+    if(val === 2) return val;
+})
+console.log(ans);
